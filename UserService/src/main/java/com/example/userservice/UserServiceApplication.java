b4108b9c1d96387f66103f31643b264d557fecfe
Priyam Omer <fsi@MacBook-Pro-2.local>
2026-04-15 22:26:20 +0530

    Testing CI and CD

--- UserService/src/main/java/com/example/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 // Testing CI and CD workflow
-// Testing CI and CD worflow 2
+// Testing CI and CD worflow 3
 @SpringBootApplication
 public class UserServiceApplication implements CommandLineRunner {
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
