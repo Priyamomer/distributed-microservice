3b3f8dbcad367fa37917ab691c15ede15b41747e
Priyam Omer <fsi@MacBook-Pro-2.local>
2026-04-22 20:50:58 +0530

    CI a n d CD Testing

--- Gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -3,7 +3,7 @@ package com.example.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//CI and CD pipeline testing 5
+//CI and CD pipeline testing 6
 @SpringBootApplication
 public class GatewayApplication {
 
